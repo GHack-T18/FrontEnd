d2ab1f67b7f10ec1e5c0c5f4c085cf68a27b4dec
AmiraHADDAD <la_haddad@esi.dz>
2024-02-23 13:14:22 +0100

    SignUp responsive

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ColS from '../components/ColS';
-import Phone from '../assets/Phone.png'
+import Phone from '../assets/Phone.png';
 
 function SignUp() {
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className='flex flex-row w-screen h-[100vh] bg-page-col '>
          <div className="flex flex-col space-y-10 sm:p-32 p-10 items-start h-screen w-full bg-dash-col rounded-tr-[30px] rounded-br-[30px]">
@@ -10,11 +24,10 @@ function SignUp() {
           <ColS />
         </div>
         <div className="h-screen w-[90%] flex  justify-end items-center">
-            <div className=" absolute right-[8%] top-16">
-                <img src={Phone} alt="Kora Logo" className="" />
+            <div className=" absolute left-[50%]  top-[10%]">
+                <img src={Phone}  alt="Kora Logo" className="" />
             </div>
         </div>
-       
     </div>
   );
 }
